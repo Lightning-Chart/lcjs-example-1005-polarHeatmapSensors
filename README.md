3ce0c5041893092ae729f87c1e476af6719d3238
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2023-08-08 11:42:27 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,69 @@
+# JavaScript Real-Time Polar Sensor Chart
+
+![JavaScript Real-Time Polar Sensor Chart](polarHeatmapSensors-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+Showcase of using Polar Heatmap to visualize a scatter data set in polar coordinates.
+
+To explain that a bit further, the example establishes a number of _sensors_ that are positioned in polar coordinates (angle + amplitude). Each sensor has a numeric measurement, which could be temperature, voltage, etc. These measurements are then _extrapolated_ to linearly ease in the gaps between the sensors. The final extrapolated data set is then visualized with a heatmap.
+
+The example simulates the sensor measurements changing in real-time (60 times per second), immediately recalculates the extrapolated heatmap data and displays it with low latency.
+
+The extrapolation algorithm can be found in the example source code.
+Essentially, it calculates a measurement value for each heatmap cell by considering the value and distance to each sensor.
+
+![Heatmap cell value extrapolation](./assets/extrapolation.png)
+
+This is a simple but robust algorithm that works for a variety of use cases.
+
+
+## API Links
+
+* [Polar chart]
+* [Polar heatmap series]
+* [Polar radial axis]
+* [Polar amplitude axis]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[Polar chart]: https://lightningchart.com/js-charts/api-documentation/v4.2.0/classes/PolarChart.html
+[Polar heatmap series]: https://lightningchart.com/js-charts/api-documentation/v4.2.0/
+[Polar radial axis]: https://lightningchart.com/js-charts/api-documentation/v4.2.0/interfaces/PolarAxisRadial.html
+[Polar amplitude axis]: https://lightningchart.com/js-charts/api-documentation/v4.2.0/classes/PolarAxisAmplitude.html
+
---
+++ src/index.js
@@ -0,0 +1,184 @@
+/*
+ * Example showcasing real-time, partial data updates to Polar Heatmap.
+ */
+const lcjs = require('@arction/lcjs')
+const { lightningChart, Themes, LUT, PalettedFill, regularColorSteps, SolidFill, ColorRGBA, PointShape } = lcjs
+
+const sensors = [
+    { angle: 0, amplitude: 0, value: Math.random() },
+    { angle: 0, amplitude: 3, value: Math.random() },
+    { angle: 30, amplitude: 3, value: Math.random() },
+    { angle: 60, amplitude: 3, value: Math.random() },
+    { angle: 90, amplitude: 3, value: Math.random() },
+    { angle: 120, amplitude: 3, value: Math.random() },
+    { angle: 150, amplitude: 3, value: Math.random() },
+    { angle: 180, amplitude: 3, value: Math.random() },
+    { angle: 210, amplitude: 3, value: Math.random() },
+    { angle: 240, amplitude: 3, value: Math.random() },
+    { angle: 270, amplitude: 3, value: Math.random() },
+    { angle: 300, amplitude: 3, value: Math.random() },
+    { angle: 330, amplitude: 3, value: Math.random() },
+    { angle: 0, amplitude: 7, value: Math.random() },
+    { angle: 30, amplitude: 7, value: Math.random() },
+    { angle: 60, amplitude: 7, value: Math.random() },
+    { angle: 90, amplitude: 7, value: Math.random() },
+    { angle: 120, amplitude: 7, value: Math.random() },
+    { angle: 150, amplitude: 7, value: Math.random() },
+    { angle: 180, amplitude: 7, value: Math.random() },
+    { angle: 210, amplitude: 7, value: Math.random() },
+    { angle: 240, amplitude: 7, value: Math.random() },
+    { angle: 270, amplitude: 7, value: Math.random() },
+    { angle: 300, amplitude: 7, value: Math.random() },
+    { angle: 330, amplitude: 7, value: Math.random() },
+    { angle: 0, amplitude: 10, value: Math.random() },
+    { angle: 30, amplitude: 10, value: Math.random() },
+    { angle: 60, amplitude: 10, value: Math.random() },
+    { angle: 90, amplitude: 10, value: Math.random() },
+    { angle: 120, amplitude: 10, value: Math.random() },
+    { angle: 150, amplitude: 10, value: Math.random() },
+    { angle: 180, amplitude: 10, value: Math.random() },
+    { angle: 210, amplitude: 10, value: Math.random() },
+    { angle: 240, amplitude: 10, value: Math.random() },
+    { angle: 270, amplitude: 10, value: Math.random() },
+    { angle: 300, amplitude: 10, value: Math.random() },
+    { angle: 330, amplitude: 10, value: Math.random() },
+]
+const resolutionSectors = 16
+const resolutionAnnuli = 10
+const amplitudeMin = 0
+const amplitudeMax = 10
+const angleStep = 360 / resolutionSectors
+const amplitudeStep = (amplitudeMax - amplitudeMin) / resolutionAnnuli
+
+const polarChart = lightningChart().Polar({
+    // theme: Themes.darkGold,
+})
+
+const themeExamples = polarChart.getTheme().examples
+if (!themeExamples) {
+    throw new Error()
+}
+
+const pointSeries = polarChart
+    .addPointSeries()
+    .setName('Sensor locations')
+    .setPointSize(10)
+    .setPointShape(PointShape.Circle)
+    .setData(sensors)
+    .setCursorEnabled(false)
+    .setAutoScrollingEnabled(false)
+
+const polarHeatmap = polarChart
+    .addHeatmapSeries({
+        sectors: resolutionSectors,
+        annuli: resolutionAnnuli,
+        amplitudeStart: amplitudeMin,
+        amplitudeEnd: amplitudeMax,
+        dataOrder: 'annuli',
+    })
+    .setName('Heatmap')
+    .setIntensityInterpolation('bilinear')
+    .setCursorInterpolationEnabled(true)
+    .setHighlightOnHover(false)
+
+// This series is intended for helping understand the example. It displays the location of heatmap cells in the chart.
+// The application extrapolates a data value for each of these locations and pushes those to the heatmap.
+const cellLocationsSeries = polarChart
+    .addPointSeries()
+    .setName('Cell locations')
+    .setPointSize(3)
+    .setPointShape(PointShape.Square)
+    .setPointFillStyle(new SolidFill({ color: ColorRGBA(0, 0, 0, 100) }))
+    .setCursorEnabled(false)
+    .setMouseInteractions(false)
+
+const palette = new PalettedFill({
+    lut: new LUT({
+        units: 'intensity',
+        steps: regularColorSteps(0, 1, themeExamples.coldHotColorPalette, { alpha: 100 }),
+        interpolate: true,
+    }),
+})
+polarHeatmap.setFillStyle(palette)
+polarChart.forEachAxis((axis) => axis.fit(false))
+
+const legend = polarChart.addLegendBox().add(polarChart)
+
+requestAnimationFrame(() => {
+    const heatmapData = new Array(resolutionAnnuli).fill(0).map((_) => new Array(resolutionSectors).fill(0))
+
+    const debugCellLocations = []
+    console.time('calc heatmap cell locations')
+    const heatmapCells = []
+    for (let iAmplitude = 0; iAmplitude < resolutionAnnuli; iAmplitude += 1) {
+        const annuliCells = []
+        heatmapCells.push(annuliCells)
+        for (let iSector = 0; iSector < resolutionSectors; iSector += 1) {
+            const angle = iSector * angleStep
+            const amplitude = iAmplitude * amplitudeStep
+            debugCellLocations.push({ angle, amplitude })
+            const cellLocationPixels = polarChart.translateCoordinate({ angle, amplitude }, polarChart.coordsRelative)
+            const weights = new Array(sensors.length).fill(0)
+            annuliCells.push({ cellLocationPixels, weights, weightsSum: 0 })
+        }
+    }
+    console.timeEnd('calc heatmap cell locations')
+    cellLocationsSeries.setData(debugCellLocations)
+
+    /**
+     * Weights only need to be calculated every time a sensor location changes.
+     * It is a very heavy operation, which should only be performed when needed.
+     */
+    const calculateWeights = () => {
+        // For each heatmap cell, calculate a weight to every sensor.
+        console.time('calc weights')
+        for (let iAmplitude = 0; iAmplitude < resolutionAnnuli; iAmplitude += 1) {
+            for (let iSector = 0; iSector < resolutionSectors; iSector += 1) {
+                const cell = heatmapCells[iAmplitude][iSector]
+                let weightsSum = 0
+                sensors.forEach((sensor, i) => {
+                    const sensorLocationPixels = polarChart.translateCoordinate(sensor, polarChart.coordsRelative)
+                    const distance = Math.sqrt(
+                        (sensorLocationPixels.x - cell.cellLocationPixels.x) ** 2 +
+                            (sensorLocationPixels.y - cell.cellLocationPixels.y) ** 2,
+                    )
+                    let weight = 1
+                    if (distance !== 0) {
+                        weight = 1 / distance ** 3
+                    }
+                    cell.weights[i] = weight
+                    weightsSum += weight
+                })
+                cell.weightsSum = weightsSum
+            }
+        }
+        console.timeEnd('calc weights')
+
+        /**
+         * Return method for refreshing heatmap with updated values.
+         */
+        return () => {
+            // Extrapolate value for each heatmap cell, considering all the sensors.
+            for (let iAmplitude = 0; iAmplitude < resolutionAnnuli; iAmplitude += 1) {
+                for (let iSector = 0; iSector < resolutionSectors; iSector += 1) {
+                    const cell = heatmapCells[iAmplitude][iSector]
+                    const sum = sensors.reduce((prev, cur, i) => prev + cur.value * cell.weights[i], 0)
+                    const value = sum / cell.weightsSum
+                    heatmapData[iAmplitude][iSector] = value
+                }
+            }
+            polarHeatmap.invalidateIntensityValues(heatmapData)
+        }
+    }
+    const refreshHeatmap = calculateWeights()
+
+    // For demo purposes, change value of every sensor randomly 60 times per second and refresh heatmap.
+    const frame = () => {
+        sensors.forEach((sensor) => {
+            sensor.value = Math.max(Math.min(sensor.value + 0.05 * (Math.random() * 2 - 1), 1), 0)
+        })
+        refreshHeatmap()
+        requestAnimationFrame(frame)
+    }
+    frame()
+})
